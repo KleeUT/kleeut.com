48c0a1e36450032d7bf775d8dbbcdd05720d6ca6
kleeut <klee.ut@gmail.com>
2020-10-03 11:03:32 +1000

    :lipstick: including links in talks

---
+++ src/components/link.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { TextColor, White } from "../colors";
+export const ExternalLink = styled.a`
+  color: ${TextColor};
+  text-decoration: underline ${White};
+`;
